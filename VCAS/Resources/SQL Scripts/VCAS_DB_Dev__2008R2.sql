7a1a7ca57d2224331105088e76ae5605311f4b59
Austin Lazarus <austin.lazarus@gmail.com>
2022-12-29 21:04:54 -0400

    Update VCAS_DB_Dev__2008R2.sql

--- VCAS/Resources/SQL Scripts/VCAS_DB_Dev__2008R2.sql
+++ VCAS/Resources/SQL Scripts/VCAS_DB_Dev__2008R2.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:248582c709354e6437b39d9ddd3a700b837b9c5d6254ca5e7295a1b1ab6b2d07
-size 1607054
+oid sha256:f28e437669ee26615f2d7ce0aa10ae2df11f269739d8fe8f67a3169773503d2a
+size 83198
