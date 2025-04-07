16bbe6587dde1b09e77b6ff2dd873d35c56e84d9
lazarusvc <austin.lazarus@gmail.com>
2025-04-07 06:58:30 -0400

    UPDATES APR25

--- VCAS/Controllers/landingController.cs
+++ VCAS/Controllers/landingController.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using VCAS.Models;
 
 namespace VCAS.Controllers
 {
 public class landingController : Controller
 {
-    // GET: Landing Page
-    // ==========================
-    public ActionResult Index()
-    {
-        return View();
+        private ModelContainer db = new ModelContainer();
+
+        // GET: Landing Page
+        // ==========================
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Landing Page Docu
+        // ==========================
+        public ActionResult docs(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VCAS_supportDocs vCAS_supportDocs = db.VCAS_supportDocs.Find(id);
+            if (vCAS_supportDocs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vCAS_supportDocs);
+        }
     }
-}
 }
